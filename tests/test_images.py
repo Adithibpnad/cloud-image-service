531322feba2de8b7b263c5aa3b2a578cb1ec2159
Adithi Bappanad <adithibappanad8@gmail.com>
2025-12-14 14:03:22 +0530

    test: add basic test structure for image APIs

---
+++ tests/test_images.py
@@ -0,0 +1,11 @@
+def test_upload_endpoint():
+    assert True
+
+def test_list_endpoint():
+    assert True
+
+def test_download_endpoint():
+    assert True
+
+def test_delete_endpoint():
+    assert True
